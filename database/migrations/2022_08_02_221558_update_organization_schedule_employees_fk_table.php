88a36c11262a576d44973166a852bd4074f11404
Farid Hidayat <e.faridhidayat@gmail.com>
2022-08-02 22:17:24 +0700

    refactor: add field organization

---
+++ database/migrations/2022_08_02_221558_update_organization_schedule_employees_fk_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('schedule_employees', function (Blueprint $table) {
+            $table->foreignUuid('organization_id')->references('id')->on('organizations')->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedule_employees');
+    }
+};
